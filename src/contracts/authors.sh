b3a9cd0a344b5b1c6a6c2805bebadfee63de0b3b
Drew Taylor <drew@infiniteinternet.ca>
2020-03-04 18:23:06 -0500

    fix authors tests

--- src/contracts/authors.sh
+++ src/contracts/authors.sh
@@ -1,8 +1,8 @@
 ligo dry-run authors.ligo --syntax pascaligo main unit 'map [
-    1n -> record [
-            author = map("tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz", 0tez);
-            approved = true
-          ]
+    "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" -> record [
+        author = map ["tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" -> 0mutez];
+        approved = true
+    ];
 ]'
 
 ligo compile-contract authors.ligo --syntax pascaligo main
