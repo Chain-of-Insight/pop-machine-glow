bf2b80157c9668c8cd0522484487cf3f61b1ec17
Drew Taylor <drew@infiniteinternet.ca>
2020-03-04 21:00:12 -0500

    shell script breaks on linker

--- src/contracts/nft.sh
+++ src/contracts/nft.sh
@@ -1,15 +1,15 @@
-
-ligo dry-run oracle.ligo --syntax pascaligo --sender=tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs main \
-'mint(record [ nftId=2n; nft=record[owner="tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz",data=0x9690bbd545bd192eb56edd1d8849f7c78ed3b238ee79b749418d20ed1aa6f5b4 ] ])'\
- 'record[
-     map [
-    1n -> record [
-           
-            owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
-            data= 0x7b15bb3dee5f8891f60cd181ff424012548a9ed5e26721eb9f6518e9dd409d9e;
-           
-          ];
-     
+ligo dry-run oracle.ligo --syntax pascaligo --sender=tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs main 'Mint(
+  record [
+    nftToMintId = 2n;
+    nftToMint = record [
+      owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
+      data = 0x9690bbd545bd192eb56edd1d8849f7c78ed3b238ee79b749418d20ed1aa6f5b4
     ];
-    
-    ]'
+  ]
+)' \
+'record [
+  owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
+  data= 0x7b15bb3dee5f8891f60cd181ff424012548a9ed5e26721eb9f6518e9dd409d9e
+]'
+
+ligo compile-contract nft.ligo --syntax pascaligo main
