17cd3c37c5ad98a295fb2713142acded854ab096
Drew Taylor <drew@infiniteinternet.ca>
2020-03-04 21:41:56 -0500

    passing dry run

--- src/contracts/nft.sh
+++ src/contracts/nft.sh
@@ -1,27 +1,21 @@
-# type actionMint is record [
-#   nftToMintId : nftId;
-#   nftToMint : nft;
-# ]
-
-# type nftId is nat;
-
-# type nft is record [
-#     owner : address;
-#     data: bytes;
-# ]
-
-ligo dry-run nft.ligo --syntax pascaligo --sender=tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs main 'Mint(
+ligo dry-run nft.ligo --syntax pascaligo --sender=tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs main \
+'Mint(
   record [
     nftToMintId = 2n;
     nftToMint = record [
-      owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
+      owner = ("tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" : address);
       data = 0x9690bbd545bd192eb56edd1d8849f7c78ed3b238ee79b749418d20ed1aa6f5b4
     ];
   ]
 )' \
 'record [
-  owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
-  data = 0x7b15bb3dee5f8891f60cd181ff424012548a9ed5e26721eb9f6518e9dd409d9e
+  nfts = map [
+    1n -> record [
+      owner = "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz";
+      data = 0x9690bbd545bd192eb56edd1d8849f7c78ed3b238ee79b749418d20ed1aa6f5b4
+    ]
+  ];
+  contractOwner = "tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs"
 ]'
 
 ligo compile-contract nft.ligo --syntax pascaligo main
