f31985934a6f8c144a8b3f3f1c1ba3f2b4e32e54
Drew Taylor <drew@infiniteinternet.ca>
2020-03-08 17:28:36 -0400

    unit test show authors

--- src/contracts/authors.sh
+++ src/contracts/authors.sh
@@ -1,7 +1,18 @@
-ligo dry-run authors.ligo --syntax pascaligo main unit 'map [
+# Author 1n -> Approved
+# Author 2n -> Approved w/ tip
+# Author 3n -> Unapproved
+ligo dry-run authors.ligo --syntax pascaligo main 'Show(unit)' 'map [
     "tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" -> record [
-        author = map ["tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" -> 0mutez];
+        author = map ["tz1cmWyycuCBdHVHVCnXbRLdKfjNSesRPJyz" -> 1000000mutez];
         approved = True
+    ];
+    "tz1UAtabHR2whB3PWAuEQcKiHzkbHsPzcmHH" -> record [
+        author = map ["tz1UAtabHR2whB3PWAuEQcKiHzkbHsPzcmHH" -> 1200000mutez];
+        approved = True
+    ];
+    "tz1UAtabHR2whB3PWAuEQcKiHzkbHsPzcmHH" -> record [
+        author = map ["tz1UAtabHR2whB3PWAuEQcKiHzkbHsPzcmHH" -> 0mutez];
+        approved = False
     ];
 ]'
 
