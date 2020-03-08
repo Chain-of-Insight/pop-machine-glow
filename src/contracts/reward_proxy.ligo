96414235e4da1e371d7fc712c83ebe79ad02ebed
Joshua Dechant <josh@codecrafting.org>
2020-03-08 00:15:14 -0500

    Add initial reward proxy contract

---
+++ src/contracts/reward_proxy.ligo
@@ -0,0 +1,164 @@
+type trusted is address
+type puzzle_id is nat
+type claim is nat
+type deposit is map (claim, tez)
+type callback_set is map (claim, set (trusted))
+
+type storage is
+  record [
+    trustedContracts  : set (trusted);
+    contractOwner     : trusted;
+    contractOracle    : trusted;
+    deposits          : map (puzzle_id, deposit);
+    callbacks         : map (puzzle_id, callback_set)
+  ]
+
+(* Deposit params *)
+type deposit_params is
+  record [
+    puzzle_id         : nat;
+    claim             : nat
+  ]
+
+(* Address params *)
+type addr_params is
+  record [
+    puzzle_id         : nat;
+    claim             : nat;
+    addr              : address
+  ]
+
+(* define return for readability *)
+type return is list (operation) * storage
+
+(* define noop for readability *)
+const noOperations : list (operation) = nil;
+
+(* Valid entry points *)
+type entry_action is
+  | SetOracle of trusted
+  | ApproveContract of trusted
+  | RejectContract of trusted
+  | AddDeposit of deposit_params
+  | GrantReward of addr_params
+
+(* Add a deposit for a claim *)
+function add_deposit (const input : deposit_params; var s : storage) : return is
+  block {
+    var puzzle_deposits : deposit :=
+      case s.deposits[input.puzzle_id] of
+        Some (d) -> d
+      | None -> (map [] : deposit)
+      end;
+
+    (* Update deposit amount *)
+    var claims : tez :=
+      case puzzle_deposits[input.claim] of
+        Some (d) -> d + Tezos.amount
+      | None -> (Tezos.amount : tez)
+      end;
+
+    puzzle_deposits[input.claim] := claims;
+    s.deposits[input.puzzle_id] := puzzle_deposits;
+  } with (noOperations, s)
+
+(* Grant reward to puzzle solver *)
+function grant_reward (const input : addr_params; var s : storage) : return is
+  block {
+    if Tezos.source =/= s.contractOracle then
+      failwith("NOPERM")
+    else skip;
+
+    (* initialize operations *)
+    var operations : list (operation) := nil;
+
+    (* Send any XTZ rewards *)
+    const puzzle_deposits : deposit =
+      case s.deposits[input.puzzle_id] of
+        Some (d) -> d
+      | None -> (map [] : deposit)
+      end;
+
+    const claims : tez =
+      case puzzle_deposits[input.claim] of
+        Some (d) -> d
+      | None -> 0tz
+      end;
+
+    if claims > 0tz then {
+      (* Create payout transaction *)
+      const receiver : contract (unit) = get_contract (input.addr);
+      const payoutOperation : operation = transaction (unit, claims, receiver);
+      operations := payoutOperation # operations;
+
+      (* Adjust ledger *)
+      puzzle_deposits[input.claim] := 0tz;
+      s.deposits[input.puzzle_id] := puzzle_deposits;
+    }
+    else skip;
+
+    (* Send NFTs *)
+    (* @TODO *)
+
+
+    (* Send contract callbacks (mint nfts, etc) *)
+    (* @TODO *)
+
+  } with (operations, s)
+
+(* Set the oracle contract *)
+function set_oracle(const addr : trusted; var s : storage) : return is
+  block {
+    (* Can only be called by contract creator *)
+    if Tezos.source =/= s.contractOwner then
+      failwith("NOPERM")
+    else skip;
+
+    (* Only a trusted contract can be an oracle *)
+    const trusted : bool = s.trustedContracts contains addr;
+    if trusted = False then
+      failwith("NOPERM")
+    else skip;
+
+    (* Update storage *)
+    s.contractOracle := addr;
+
+  } with (noOperations, s)
+
+(* Approve a trusted contract for reward disbursement *)
+function approve_contract (const addr : trusted; var s : storage) : return is
+  block {
+    (* Can only be called by contract creator *)
+    if Tezos.source =/= s.contractOwner then
+      failwith("NOPERM")
+    else skip;
+
+    (* Add to set *)
+    s.trustedContracts := Set.add (addr, s.trustedContracts)
+
+  } with (noOperations, s)
+
+(* Reject a trusted contract *)
+function reject_contract (const addr : trusted; var s : storage) : return is
+  block {
+    (* Can only be called by contract creator *)
+    if Tezos.source =/= s.contractOwner then
+      failwith("NOPERM")
+    else skip;
+
+    (* Remove from set *)
+    s.trustedContracts := Set.remove (addr, s.trustedContracts)
+
+  } with (noOperations, s)
+
+(* Main entrypoint *)
+function main (const action : entry_action; var s : storage) : return is
+  block {
+    skip
+  } with case action of
+    | SetOracle(param) -> set_oracle(param, s)
+    | ApproveContract(param) -> approve_contract(param, s)
+    | RejectContract(param) -> reject_contract(param, s)
+    | AddDeposit(param) -> add_deposit(param, s)
+    | GrantReward(param) -> grant_reward(param, s)
+  end;
---
+++ src/contracts/test_reward_proxy.py
@@ -0,0 +1,219 @@
+from os.path import dirname, join
+from unittest import TestCase
+import sys, hashlib
+from decimal import Decimal
+
+from pytezos import ContractInterface, pytezos, format_timestamp, MichelsonRuntimeError
+
+# testcases
+class RewardProxyTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.proxy = ContractInterface.create_from(join(dirname(__file__), 'build/reward_proxy.tz'), shell='sandboxnet')
+        cls.maxDiff = None
+
+
+    def test_source_can_approve(self):
+        storage = {
+            "trustedContracts": [],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .approveContract('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_anyone_cannot_approve(self):
+        storage = {
+            "trustedContracts": [],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        with self.assertRaises(MichelsonRuntimeError):
+            res = self.proxy \
+                .approveContract('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+                .result(storage=storage, sender="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", source="tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs")
+
+    def test_approve_multiple(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .approveContract('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD", "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_add_initial_deposit(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .addDeposit(puzzle_id=1583093350498, claim=1) \
+            .with_amount(100000) \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {1583093350498: {1: Decimal('0.1')}},
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_add_subsequent_deposit(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {1583093350498: {1: Decimal('0.1')}},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .addDeposit(puzzle_id=1583093350498, claim=1) \
+            .with_amount(200000) \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {1583093350498: {1: Decimal('0.3')}},
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_add_deposit_multiple_puzzle(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {1583093350498: {1: Decimal('0.1')}},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .addDeposit(puzzle_id=1583258505553, claim=1) \
+            .with_amount(1500000) \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {
+                1583093350498: {1: Decimal('0.1')},
+                1583258505553: {1: Decimal('1.5')}
+            },
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_add_deposit_multiple_claim(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {1583093350498: {1: Decimal('0.1')}},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .addDeposit(puzzle_id=1583093350498, claim=2) \
+            .with_amount(50000) \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "deposits": {
+                1583093350498: {1: Decimal('0.1'), 2: Decimal('0.05')}
+            },
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_source_can_set_trusted_oracle(self):
+        storage = {
+            "trustedContracts": [ "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don",
+            "deposits": {},
+            "callbacks": {}
+        }
+        res = self.proxy \
+            .setOracle('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+            .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+        expected = {
+            "trustedContracts": [ "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD",
+            "deposits": {},
+            "callbacks": {}
+        }
+        self.assertDictEqual(expected, res.storage)
+
+
+    def test_source_cannot_set_untrusted_oracle(self):
+        storage = {
+            "trustedContracts": [ "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don",
+            "deposits": {},
+            "callbacks": {}
+        }
+
+        with self.assertRaises(MichelsonRuntimeError):
+            res = self.proxy \
+                .setOracle('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+                .result(storage=storage, source="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+
+
+    def test_anyone_cannot_set_oracle(self):
+        storage = {
+            "trustedContracts": [ "KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD" ],
+            "contractOwner": "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+            "contractOracle": "KT1RUT25eGgo9KKWXfLhj1xYjghAY1iZ2don",
+            "deposits": {},
+            "callbacks": {}
+        }
+        with self.assertRaises(MichelsonRuntimeError):
+            res = self.proxy \
+                .setOracle('KT1JepfBfMSqkQyf9B1ndvURghGsSB8YCLMD') \
+                .result(storage=storage, sender="tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", source="tz1WtSgQKTpHUNfXwGFKQtXfphZWkLE2FPCs")
